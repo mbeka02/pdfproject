2d3a48b072f63c1bb6d56066cd302336d31fb4da
mbeka02 <antonymbeka@gmail.com>
2023-08-07 17:51:59 +0300

    testing

---
+++ src/Image.tsx
@@ -0,0 +1,40 @@
+import { motion } from "framer-motion";
+import icon from "./images/IMG_20230725_152854_464.jpg";
+import { useState } from "react";
+
+type Props = {
+  layer_x: number;
+  layer_y: number;
+};
+
+const Customcomponent = ({ layer_x, layer_y }: Props) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [isResizing, setIsResizing] = useState(false);
+  const getImagePositionRelativeToPage = (
+    e: MouseEvent | TouchEvent | PointerEvent
+  ) => {
+    const image = e.target as HTMLImageElement;
+    const rect = image.getBoundingClientRect();
+    const x = rect.left - layer_x;
+    const y = rect.top - layer_y;
+    console.log(x, y);
+  };
+  return (
+    <motion.img
+      src={icon}
+      className="img"
+      id="draggable"
+      style={{
+        zIndex: 100,
+      }}
+      drag
+      //track x and y coordinates
+      onDrag={(event, _) => {
+        getImagePositionRelativeToPage(event);
+      }}
+
+      //limit to the page width and document height
+    />
+  );
+};
+export default Customcomponent;
--- src/Plugin.tsx
+++ src/Plugin.tsx
@@ -1,47 +1,10 @@
 import { Plugin, PluginOnAnnotationLayerRender } from "@react-pdf-viewer/core";
 //import { createRoot } from "react-dom/client";
+import Customcomponent from "./Image";
 
 // Plugins
 
-import icon from "./images/IMG_20230725_152854_464.jpg";
-
-import { motion } from "framer-motion";
-
 import { createRoot } from "react-dom/client";
-
-type Props = {
-  layer_x: number;
-  layer_y: number;
-};
-
-const Customcomponent = ({ layer_x, layer_y }: Props) => {
-  const getImagePositionRelativeToPage = (
-    e: MouseEvent | TouchEvent | PointerEvent
-  ) => {
-    const image = e.target as HTMLImageElement;
-    const rect = image.getBoundingClientRect();
-    const x = rect.left - layer_x;
-    const y = rect.top - layer_y;
-    console.log(x, y);
-  };
-  return (
-    <motion.img
-      src={icon}
-      className="img"
-      id="draggable"
-      style={{
-        zIndex: 100,
-      }}
-      drag
-      //track x and y coordinates
-      onDrag={(event, _) => {
-        getImagePositionRelativeToPage(event);
-      }}
-
-      //limit to the page width and document height
-    />
-  );
-};
 
 const customPlugin = (): Plugin => {
   const onAnnotationLayerRender = (e: PluginOnAnnotationLayerRender) => {
@@ -56,7 +19,7 @@ const customPlugin = (): Plugin => {
       ".rpv-core__annotation-layer"
     ) as NodeListOf<HTMLElement>;
 
-    annotationLayers.forEach((layer, index) => {
+    annotationLayers.forEach((layer) => {
       //render annotation layer with the draggableImage component
       layer.style.border = "1px red solid";
       const pos = layer.getBoundingClientRect();
