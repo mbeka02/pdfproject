7e099cc7d53fd2924646c0c009f5c5c610b83692
mbeka02 <antonymbeka@gmail.com>
2023-08-09 17:55:54 +0300

    working on e2e tests

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -4,23 +4,26 @@ import { test, expect } from "@playwright/test";
 //double click and drag to resize
 //coordinates of the image ? not sure on this one
 
-//work in progress
-//seems to be working fine but the image clipping behind the page issue is still there
-test("test", async ({ page }) => {
+//testing drag functonality
+test("drag image to another page", async ({ page }) => {
   await page.goto("https://pdfproject.vercel.app/");
   //never wait for timeout in production
   // await page.waitForTimeout(1000);
-  //get the image
+  //get the image from the annotation layer , need to find a way to randomize the image selection
   const image = page
     .getByTestId("core__annotation-layer-1")
     .locator("#draggable");
-  //drag the image to the previous page annotation layer
+  //handle error if image not found
+  if (!image) {
+    throw new Error("image not found");
+  }
+  //get the destination page
   const destinationPage = page.getByTestId("core__annotation-layer-0");
   if (destinationPage) {
-    //could be more specific
+    //drag the image to the destination page
     await image.dragTo(destinationPage);
+    //check if the image is in the destination page
+    //may not be needed
+    await expect(destinationPage).toHaveId("draggable");
   }
-  //double click the image
-
-  //await page.getByTestId('core__annotation-layer-0').locator('#draggable').dblclick();
 });
